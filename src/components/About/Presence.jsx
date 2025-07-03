e1ae48749e603db64db36913b57546aeecae4f7c
ananthu <heyananthulal@gmail.com>
2025-07-03 15:14:19 +0530

    about section added

--- src/components/About/Presence.jsx
+++ src/components/About/Presence.jsx
@@ -3,9 +3,9 @@ import map from '../../assets/about-map.png'
 function Presence() {
     return (
         <div className='font-questrial flex w-full  h-full bg-gray-100 py-2 lg:pb-36'>
-            <div className='flex flex-col lg:flex-row p-4 '>
+            <div className='flex flex-col lg:flex-row p-4 md:pl-16'>
                 <div className=''>
-                    <img src={map} alt="" className='w-full lg:w-[84rem] ' />
+                    <img src={map} alt="" className='w-full lg:w-[80rem] ' />
                 </div>
                 <div className='flex items-start justify-center'>
                     <div className='py-28'>
