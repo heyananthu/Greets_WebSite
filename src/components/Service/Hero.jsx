6d7cd6398a05106eea60e3f37cdf776c6e067112
ananthu <heyananthulal@gmail.com>
2025-07-11 09:53:31 +0530

    services page responsive testing

--- src/components/Service/Hero.jsx
+++ src/components/Service/Hero.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Hero() {
     return (
-        <div className='font-questrial mt-28 bg-gray-100'>
+        <div className='font-questrial mt-24 bg-gray-100'>
             <div className='pt-12 w-full lg:w-[78%] lg:pl-16 p-4 space-y-4'>
-                <h1 className='text-8xl '>Services</h1>
-                <p className='text-4xl'>Helping you make the right decision.</p>
-                <p className='text-xl'>At GREETS, our services go beyond blueprints and wiring. We believe in creating intelligent, future-ready environments that align with your goals—whether they’re operational efficiency, sustainability, or visual impact. Our team combines technical excellence with hands-on experience to offer full-spectrum MEP, architectural, and project execution services. Every solution is customized, every detail is engineered for success.</p>
+                <h1 className='md:text-8xl text-7xl'>Services</h1>
+                <p className='md:text-4xl text-3xl'>Helping you make the right decision.</p>
+                <p className='md:text-xl text-lg'>At GREETS, our services go beyond blueprints and wiring. We believe in creating intelligent, future-ready environments that align with your goals—whether they’re operational efficiency, sustainability, or visual impact. Our team combines technical excellence with hands-on experience to offer full-spectrum MEP, architectural, and project execution services. Every solution is customized, every detail is engineered for success.</p>
             </div>
 
         </div>
