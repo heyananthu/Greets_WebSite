541c99ca2c6d466aa6dca2e8b2b1aa89920c78f8
ananthu <heyananthulal@gmail.com>
2025-07-12 10:32:40 +0530

    map image changed on about section

--- src/components/About/Presence.jsx
+++ src/components/About/Presence.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import map from '../../assets/about-map.png'
+import map from '../../assets/about-map-new.png'
 function Presence() {
     return (
-        <div className='font-questrial flex w-full  h-full bg-gray-100 py-2 lg:pb-36'>
+        <div className='font-questrial flex w-full  h-full bg-gray-50 py-2 lg:pb-36'>
             <div className='flex flex-col lg:flex-row p-4 md:pl-16'>
                 <div className=''>
                     <img src={map} alt="" className='w-full lg:w-[80rem] ' />
