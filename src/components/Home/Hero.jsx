31100726ffc8331bf12a943be0be721e1e5c356f
ananthu <heyananthulal@gmail.com>
2025-07-27 19:53:04 +0530

    hero section banner video added

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
-import banner from '../../assets/hero-banner.jpg'
+import heroVideo from '../../assets/video/banner_video.mp4'
 
 function Hero() {
     return (
-        <div className="px-4 sm:px-8 lg:px-16 mb-16 ">
-            <div
-                className="w-full h-[15rem] bg-cover bg-center md:h-[18rem] md:w-full  sm:h-[24rem] lg:h-[32rem] mt-24"
-                style={{ backgroundImage: `url(${banner})` }}
-            >
-                <div className="p-4">
-                    <h1 className="text-3xl mt-6 sm:text-5xl lg:text-[5rem] text-white lg:leading-[6rem] font-questrial [text-shadow:_0_4px_12px_rgba(0,0,0,1)]">
+        <div className=" mb-16 relative">
+            <div className="w-full h-[15rem] md:h-[18rem] sm:h-[24rem] lg:h-[32rem] mt-24 relative overflow-hidden">
+                {/* Video Background */}
+                <video
+                    className="absolute top-0 left-0 w-full h-full object-cover z-0"
+                    src={heroVideo}
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                />
+                {/* Gray Overlay */}
+                <div className="absolute top-0 left-0 w-full h-full bg-cyan-400/20 z-5"></div>
+                {/* Bottom White Gradient Overlay */}
+                <div className="absolute bottom-0 left-0 w-full h-48 z-10 pointer-events-none bg-gradient-to-t from-white to-transparent"></div>
+                {/* Overlay Content */}
+                <div className="p-4 relative z-20 lg:px-16 flex md:items-center items-start">
+                    <h1 className="text-3xl mt-4 sm:text-5xl lg:text-[5.5rem] text-white lg:leading-[6rem] font-questrial [text-shadow:_0_4px_12px_rgba(0,0,0,1)]">
                         Transforming the Built  <br className="hidden md:block" />
-                        Environment with Expert MEP <br className="hidden md:block" />
-                        Design & Sustainable Solutions
+                        Environment with Expert <br className="hidden md:block" /> MEP 
+                        Design & Sustainable <br className="hidden md:block" /> Solutions
                     </h1>
-
                 </div>
             </div>
         </div>
--- src/components/Home/TransformingSolution.jsx
+++ src/components/Home/TransformingSolution.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import banner from '../../assets/secondbanner.jpg';
+import banner from '../../assets/transform.avif';
 
 function TransformingSolution() {
     return (
