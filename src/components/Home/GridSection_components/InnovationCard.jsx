79c9421ac5ae36fa28de15b7e0d99c796b63e90c
ananthu <heyananthulal@gmail.com>
2025-08-01 19:26:43 +0530

    grid section again again changed

--- src/components/Home/GridSection_components/InnovationCard.jsx
+++ src/components/Home/GridSection_components/InnovationCard.jsx
@@ -20,7 +20,7 @@ function InnovationCard() {
                     <p className='lg:text-lg text-sm lg:mt-12'>
                         Our team thrives on delivering results that balance ecology, economy, and efficiency. Headquartered in India, with a presence in the Maldives, Mauritius, Seychelles, Gambia, and other tropical regions, we manage complex international projects with precision and care.
                     </p>
-                    <p className='lg:text-xl text-sm lg:mt-6 mt-1'>
+                    <p className='lg:text-lg text-sm lg:mt-6 mt-1'>
                         With deep expertise across 20+ service verticals, we partner with clients from concept to commissioning—ensuring every project is smartly designed, precisely built, and future-ready. Our commitment: delivering engineered environments that are efficient, ecological, and built to last.
                     </p>
                     <div className="mt-28">
