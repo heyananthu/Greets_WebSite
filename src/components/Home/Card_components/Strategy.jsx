54abf8d3eb83af0be00c687535c40049107879bb
ananthu <heyananthulal@gmail.com>
2025-08-02 08:51:48 +0530

    grid section again again changed

--- src/components/Home/Card_components/Strategy.jsx
+++ src/components/Home/Card_components/Strategy.jsx
@@ -13,8 +13,8 @@ function Strategy() {
                 </div>
 
                 {/* Right Side */}
-                <div className="flex-1 flex flex-col items-center justify-center px-6 py-8 mb-12 md:mb-16 bg-white h-full lg:-mt-5">
-                    <h2 className="text-3xl  font-bold text-black mb-1 lg:mb-4 font-questrial">QUALITY</h2>
+                <div className="flex-1 flex flex-col items-center justify-center px-6 py-8 mb-12 md:mb-16 bg-white h-full ">
+                    <h2 className="text-3xl  font-bold text-black mb-1 lg:mb-4 font-questrial lg:-mt-14">QUALITY</h2>
                     <p className='italic md:text-md text-sm'>Built to precision. Delivered with purpose.</p>
                     <p className="text-green-700 text-md md:text-lg max-w-sm lg:mt-6 mt-2 text-center">
                     Our commitment to quality is built into every step—from concept to commissioning. Backed by certified professionals and rigorous QA/QC protocols, GREETS ensures that every system we deliver meets global benchmarks for reliability, safety, and performance.</p>
